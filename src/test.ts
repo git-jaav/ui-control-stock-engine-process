d215e48a55ee624ce839af0a69c29ed68e56747c
git-jaav <araucovillar@gmail.com>
2026-03-08 23:17:13 -0500

    general arquetipe of angular 21

---
+++ src/test.ts
@@ -0,0 +1,7 @@
+// This file is required by karma.conf.js and is used for test setup.
+// You can add global test helpers here.
+
+import 'zone.js/testing';
+
+// Prevent Angular from complaining about missing TestBed configuration in some test setups.
+Object.defineProperty(window, '__karma__', { value: null });
